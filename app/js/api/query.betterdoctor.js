21a05aabf9bdb7bc3951629330227ad8ab8c4ada
Ryan G Ferris <ryanglenferris@gmail.com>
2018-02-16 14:56:10 -0800

    Portland, Oregon
    Uopdate the API query to display results for te Portland, Oregon region.

--- app/js/api/query.betterdoctor.js
+++ app/js/api/query.betterdoctor.js
@@ -3,8 +3,8 @@ const api_key = require('./../../../.env').apiKey;
 export let queryBetterDoctor = function (doctor_name, medical_issue) {
 	return new Promise(function (resolve, reject) {
 		let request = new XMLHttpRequest();
-		let location = "37.773%2C-122.413%2C100";
-		let user_location = "37.773%2C-122.413";
+		let location = "45.5231%2C-122.6765%2C100";
+		let user_location = "45.5231%2C-122.6765";
 
 		let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${doctor_name}&query=${medical_issue}&location=${location}&user_location=${user_location}&skip=0&user_key=${api_key}`
 		request.onload = function () {
