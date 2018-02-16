9485e5cd7f2872b7bc4f8e4d92624ee561ee87d4
Ryan G Ferris <ryanglenferris@gmail.com>
2018-02-16 09:31:10 -0800

    index.interface.js
    Create the basic index.interface.js file layout.

---
+++ app/js/src/index.interface.js
@@ -0,0 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { api_query } from './../api/query.betterdoctor.js';
+
+$(document).ready(function () {
+
+	/* * * * * * * * * * * FORM SUBMIT * * * * * * * * * * */
+	$('#better-doctor-query').submit(function (event) {
+
+	});
+});
