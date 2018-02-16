eb587d4a18025191ce8c50d7ac7ec60db52fc31c
Ryan G Ferris <ryanglenferris@gmail.com>
2018-02-16 13:47:29 -0800

    API Query
    Add code to query for phone numbers, websites, addresses, and whether or
    not a clinic is accepting new patients at this time.

--- app/js/src/index.js
+++ app/js/src/index.js
@@ -12,25 +12,37 @@ $(document).ready(function () {
 
 		query.then(function (response) {
 			let body = JSON.parse(response);
+			console.log('SEARCH Clicked');
 
-			Object.keys(body.data).forEach(function (key) {
-				let profile = body.data[key].profile;
-				let first_name = body.data[key].profile.first_name;
-				let last_name = body.data[key].profile.last_name;
+			Object.keys(body.data).forEach(function (key_profile) {
+				/* PROFILE */
+				let profile = body.data[key_profile].profile;
+				let first_name = body.data[key_profile].profile.first_name;
+				let last_name = body.data[key_profile].profile.last_name;
 				console.log(`${last_name}, ${first_name}`);
+				/* PRACTICES */
+				Object.keys(body.data[key_profile].practices).forEach(function (key_practice) {
+					//Address
+					let address_street = body.data[key_profile].practices[key_practice].visit_address.street;
+					let address_city = body.data[key_profile].practices[key_practice].visit_address.city;
+					let address_state = body.data[key_profile].practices[key_practice].visit_address.state;
+					let address_state_long = body.data[key_profile].practices[key_practice].visit_address.state_long;
+					let address_zip = body.data[key_profile].practices[key_practice].visit_address.zip;
+					console.log(`${address_street}, ${address_city}, ${address_state_long} ${address_zip}`);
+					//Accepting new patients
+					let patients = body.data[key_profile].practices[key_practice].accepts_new_patients;
+					console.log(`Accepting new patients: ${patients}`);
+					//Website
+					let website = body.data[key_profile].practices[key_practice].website;
+					console.log(`Website: ${website}`);
+					/* PHONE */
+					Object.keys(body.data[key_profile].practices[key_practice].phones).forEach(function (key_phone) {
+						let phone_type = body.data[key_profile].practices[key_practice].phones[key_phone].type;
+						let phone_number = body.data[key_profile].practices[key_practice].phones[key_phone].number;
+						console.log(`${phone_type}: ${phone_number}`);
+					});
+				});
 			});
-
-			/*
-
-			let address
-			let phone number
-			let website
-			let accepting new patients
-			*/
-
-			//for
-			// for (i = 0; i < body.length);
-			//each
 
 		}, function (error) {
 			$('.showErrors').text(`There was an error processing your request: ${error.message}`);
