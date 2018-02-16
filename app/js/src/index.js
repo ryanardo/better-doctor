23c005fb8bf239dbd3e830c37bcad9642d9ce73a
Ryan G Ferris <ryanglenferris@gmail.com>
2018-02-16 14:34:03 -0800

    Query Results
    Update jQuery in the index.js file to display the API query results.

--- app/js/src/index.js
+++ app/js/src/index.js
@@ -9,6 +9,7 @@ $(document).ready(function () {
 		let doctor_name = $("#doctor-name").val();
 		let medical_issue = $("#medical-issue").val();
 		let query = queryBetterDoctor(doctor_name, medical_issue);
+		$("#query-results").empty();
 
 		query.then(function (response) {
 			let body = JSON.parse(response);
@@ -19,7 +20,7 @@ $(document).ready(function () {
 				let profile = body.data[key_profile].profile;
 				let first_name = body.data[key_profile].profile.first_name;
 				let last_name = body.data[key_profile].profile.last_name;
-				console.log(`${last_name}, ${first_name}`);
+				$("#query-results").append(`<h1>${first_name} ${last_name}</h1>`);
 				/* PRACTICES */
 				Object.keys(body.data[key_profile].practices).forEach(function (key_practice) {
 					//Address
@@ -28,22 +29,27 @@ $(document).ready(function () {
 					let address_state = body.data[key_profile].practices[key_practice].visit_address.state;
 					let address_state_long = body.data[key_profile].practices[key_practice].visit_address.state_long;
 					let address_zip = body.data[key_profile].practices[key_practice].visit_address.zip;
-					console.log(`${address_street}, ${address_city}, ${address_state_long} ${address_zip}`);
+					$("#query-results").append(`${address_street}, ${address_city}, ${address_state_long} ${address_zip}`);
 					//Accepting new patients
 					let patients = body.data[key_profile].practices[key_practice].accepts_new_patients;
-					console.log(`Accepting new patients: ${patients}`);
+					$("#query-results").append(`<p>Accepting new patients: ${patients}</p>`);
 					//Website
 					let website = body.data[key_profile].practices[key_practice].website;
-					console.log(`Website: ${website}`);
+
+					if (website === undefined) {
+						$("#query-results").append(`No website available for this clinic.`);
+					} else {
+						$("#query-results").append(`<a href="${website}">${website}</a>`);
+					}
+
 					/* PHONE */
 					Object.keys(body.data[key_profile].practices[key_practice].phones).forEach(function (key_phone) {
 						let phone_type = body.data[key_profile].practices[key_practice].phones[key_phone].type;
 						let phone_number = body.data[key_profile].practices[key_practice].phones[key_phone].number;
-						console.log(`${phone_type}: ${phone_number}`);
+						$("#query-results").append(`<p>${phone_type}: ${phone_number}</p>`);
 					});
 				});
 			});
-
 		}, function (error) {
 			$('.showErrors').text(`There was an error processing your request: ${error.message}`);
 		});
